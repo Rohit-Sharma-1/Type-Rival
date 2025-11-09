4aa7c497133477e84d974722c5e96fa7ee4c4416
Rohit Sharma <rohitrohan2424@gmail.com>
2025-11-10 03:01:18 +0530

    Done

--- 1.Front/src/components/Socket.tsx
+++ 1.Front/src/components/Socket.tsx
@@ -2,6 +2,6 @@
 //https://type-rival.onrender.com
 
 import { io } from "socket.io-client";
-export const socket = io("http://localhost:3000", {
+export const socket = io("https://type-rival.onrender.com", {
   autoConnect: true,
 });
